ca4432567b19adcff62bfe772f72ffaa6609d07d
Sergio Henrique <sergiohenriquetp@gmail.com>
2014-05-31 18:33:00 -0300

    refatorando funcao javascript

--- js/index.js
+++ js/index.js
@@ -1,57 +1,24 @@
-$(function() {
-  $("#alfabeto div").draggable(
+$("#alfabeto div").draggable(
     {helper: "clone"}
-  );
+);
 
-  $( "#droppable_D0" ).droppable({
-    accept: "#draggable_D",
+function make_droppable(letter, div_id){
+  str = "#draggable_" + letter;
+  $(div_id).droppable({
+    accept: str,
     hoverClass: "ui-state-active",
     drop: function( event, ui ) {
       $( this )
         .addClass( "ui-state-highlight" )
         .find( "p" )
-          .html("<img src=alfabeto/AlfaD.jpg width=50px />" )
+          .html("<img src=alfabeto/Alfa" + letter + ".jpg width=50px />" )
         .find("campos")
           $('div#campos').append("")
     }
   });
+}
 
-  $( "#droppable_A1" ).droppable({
-    accept: "#draggable_A",
-    hoverClass: "ui-state-active",
-    drop: function( event, ui ) {
-      $( this )
-        .addClass( "ui-state-highlight" )
-        .find( "p" )
-          .html("<img src=alfabeto/AlfaA.jpg width=50px />" )
-        .find("campos")
-          $('div#campos').append("")
-    }
-  });
-
-  $( "#droppable_V2" ).droppable({
-    accept: "#draggable_V",
-    hoverClass: "ui-state-active",
-    drop: function( event, ui ) {
-      $( this )
-        .addClass( "ui-state-highlight" )
-        .find( "p" )
-          .html("<img src=alfabeto/AlfaV.jpg width=50px />" )
-        .find("campos")
-          $('div#campos').append("")
-    }
-  });
-
-  $( "#droppable_I3" ).droppable({
-    accept: "#draggable_I",
-    hoverClass: "ui-state-active",
-    drop: function( event, ui ) {
-      $( this )
-        .addClass( "ui-state-highlight" )
-        .find( "p" )
-          .html("<img src=alfabeto/AlfaI.jpg width=50px />" )
-        .find("campos")
-          $('div#campos').append("<a href=jogo.php?x=1200&grupo=4&dificuldade=&Jogar=Jogar><div id=finalizar></div><div id=conteudo><p style=float:left; width:300px;>PARABENS!<BR /><BR />VOC&Ecirc; ACERTOU!</p><img src=imagens/fotos/4-DAVI.jpg width=350px height=270px style=float:right;/></div></a>")
-      }
-    });
-  });
+make_droppable('D', '#droppable_D0');
+make_droppable('A', '#droppable_A1');
+make_droppable('V', '#droppable_V2');
+make_droppable('I', '#droppable_I3');
